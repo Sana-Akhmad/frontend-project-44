bc8781fc1ab64e1be7cb2be640fb90b29802d454
Vacheslav <89138372+GSlav080@users.noreply.github.com>
2023-04-24 22:53:27 +1000

    Update hexlet-check.yml

--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,6 @@ import readlineSync from 'readline-sync';
 export  function send()
 {   console.log("Welcome to the Brain Games!")
     const name = readlineSync.question('May I have your name? : ');
-    console.log("Hello, "+name)}
+    console.log("Hello, "+name);
+    return name;
+}
